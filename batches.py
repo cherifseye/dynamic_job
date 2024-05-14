99876265a58f64e5b3e2fef6b32459d65b592f68
cherifseye <cherifseye2018@gmail.com>
2024-05-14 16:00:30 -0400

    refactoring batches.py file

--- batches.py
+++ batches.py
@@ -3,19 +3,21 @@ import json
 
 
 def generate_test_batches():
-  list_jobs_dict = []
+  test_jobs_list = []
   test_folder = "tests"
+
+  #Obtenir une liste des chemins des fichiers de test du dossier 
   test_files = [os.path.join(test_folder, f) for f in os.listdir(test_folder) if f.startswith("test")]
+
   if test_files:
+    #Pour chaque fichiers de test generer un nom de lot de batch
     for i, test_file in enumerate(test_files):
       output_name = f"TEST_BATCH_{i+1}"
-      dict_job = {}
-      dict_job["name"] = output_name
-      dict_job["path"] = test_file
-      list_jobs_dict.append(dict_job)
-
+      test_jobs_list.append({"name": output_name, "path": test_file})
+  
+  #ouvrir le fichier GITHUB_OUTPUT en mode ajout et ecrire en format json l'output pour la matrice.
   with open(os.environ["GITHUB_OUTPUT"], 'a') as f:
-    f.write(f"test_batches={json.dumps(list_jobs_dict)}")
+    f.write(f"test_batches={json.dumps(test_jobs_list)}")
 
 if __name__ == "__main__":
   generate_test_batches()
