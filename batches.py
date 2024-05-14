ed443dc3486d52a72794b86a102d425856484b67
cherifseye <cherifseye2018@gmail.com>
2024-05-14 13:57:59 -0400

    fix

--- batches.py
+++ batches.py
@@ -14,7 +14,7 @@ def generate_test_batches():
       dict_job["path"] = test_file
       list_jobs_dict.append(dict_job)
       
-  with open(os.environ["GITHUB_OUTPUT"], 'w') as f:
+  with open(os.environ["GITHUB_OUTPUT"], 'a') as f:
     f.write(f"test_batches = {json.dumps(list_jobs_dict)}")
 
 if __name__ == "__main__":
